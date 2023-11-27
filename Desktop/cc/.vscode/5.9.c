ed9aaa75442877306be0a7045d260db9c1ee9b8a
yu666yu666yu666 <1759753124@qq.com>
2023-11-27 19:15:43 +0800

    h

---
+++ Desktop/cc/.vscode/5.9.c
@@ -0,0 +1,26 @@
+/* 5.9 */
+#include <stdio.h>
+#include <stdlib.h>
+void Temperatures(double t);
+
+int main()
+{
+    double t;
+    printf("Please input a temperature: ");
+    while (scanf("%lf", &t) == 1)
+    {
+        Temperatures(t);
+        printf("input again: ");
+    }
+    return 0;
+}
+
+void Temperatures(double t)
+{
+    const double w0 = 5.0 / 9.0;
+    const double w1 = 32.0;
+    const double k = 273.16;
+    printf("%.2lf\n", t);
+    printf("%.2lf\n", w0 * (t - w1));
+    printf("%.2lf\n", k + w0 * (t - w1));
+}
---
+++ Desktop/cc/6.4.c
@@ -0,0 +1,18 @@
+/* 6.4  */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    char a='A';
+    int m=0;
+    for(int i=0;i<6;i++)
+    {
+        for(int j=0;j<i+1;j++)
+        {
+            printf("%c",a+m);
+            m++;
+        }
+        printf("\n");
+    }
+}
