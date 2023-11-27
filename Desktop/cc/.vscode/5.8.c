fb4cb5378447913795e42693115f76487cc2d982
yu666yu666yu666 <1759753124@qq.com>
2023-11-27 17:43:41 +0800

    h

---
+++ Desktop/cc/.vscode/5.8.c
@@ -0,0 +1,17 @@
+/* 5.8 */
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int mod,num;
+    printf("This program computes moduli.\nEnter an integer to serve as the second operand: ");
+    scanf("%d", &mod);
+    printf("Now enter the first operand: ");
+    while (scanf("%d", &num) && num > 0)
+    {
+        printf("%d %% %d is %d\n", num, mod, num % mod);
+        printf("Enter next number for first operand (<= 0 to quit): ");
+    }
+    printf("Done\n");
+    return 0;
+}
