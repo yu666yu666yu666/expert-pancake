366a631eb4691f4e72c7a1f1519882aba87d09bc
yu666yu666yu666 <1759753124@qq.com>
2023-11-27 20:23:08 +0800

    h

---
+++ Desktop/cc/6.18.c
@@ -0,0 +1,15 @@
+/* 6.18 */
+#include <stdio.h>
+#include <stdlib.h>
+int main ()
+{
+    int n=5;
+    int i=1;
+    while((n-i)*2<=150)
+    {
+        n=(n-i)*2;
+        i++;
+        printf("%d\n",n);
+    }
+    return 0;
+}
