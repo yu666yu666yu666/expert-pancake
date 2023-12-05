e33d0953383218af2a5802c0925cad4b1aaeb97d
yu666yu666yu666 <1759753124@qq.com>
2023-12-05 21:36:51 +0800

    h

---
+++ Desktop/cc/ke.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    int i;
+    int *p=(int *)malloc(4*sizeof(int));
+    int *q;
+    for(i=0,q=p;i<4;i++,q++)
+    {
+        scanf("%d",q);
+    }
+    for(i=,q=p;i<4;i++,q++)
+    {
+        printf("%d",*q);
+    }
+    return 0;
+}
