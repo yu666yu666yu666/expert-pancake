16743c88ba4089c1160c8dae6fdf73b53168b8f1
yu666yu666yu666 <1759753124@qq.com>
2023-12-08 22:39:11 +0800

    h

--- Desktop/cc/ke.c
+++ Desktop/cc/ke.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include<math.h>
 int main ()
 {
     int i;
-    int *p=(int *)malloc(6*sizeof(int));
+    int *p=(int *)malloc(2*sizeof(int));
     int *q;
-    for(i=0,q=p;i<6;i++,q++)
+    for(i=0,q=p;i<2;i++,q++)
     {
         scanf("%d",q);
     }
-    for(i=0,q=p;i<6;i++,q++)
+    for(i=0,q=p;i<2;i++,q++)
     {
         printf("%d",*q);
     }
