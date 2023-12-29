c9ba4445a9aa8cdb15ae8104777f67a546cf31d1
yu666yu666yu666 <1759753124@qq.com>
2023-12-29 15:57:21 +0800

    h

--- Desktop/cc/ke.c
+++ Desktop/cc/ke.c
@@ -5,13 +5,13 @@
 int main ()
 {
     int i;
-    int *p=(int *)malloc(6*sizeof(int));
+    int *p=(int *)malloc(5*sizeof(int));
     int *q;
-    for(i=0,q=p;i<6;i++,q++)
+    for(i=0,q=p;i<5;i++,q++)
     {
         scanf("%d",q);
     }
-    for(i=0,q=p;i<6;i++,q++)
+    for(i=0,q=p;i<5;i++,q++)
     {
         printf("%d",*q);
     }
